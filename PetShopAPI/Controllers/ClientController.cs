82cdff39ef4139b45d7a2aba5d361c076a2a50ca
julys martins <julystube@gmail.com>
2026-07-26 17:24:09 -0300

    feat:add client controller

--- PetShopAPI/Controllers/ClientController.cs
+++ PetShopAPI/Controllers/ClientController.cs
@@ -7,11 +7,46 @@ namespace PetShopAPI.Controllers
   [ApiController]
   public class ClientController : ControllerBase
   {
-    [HttpGet]
-    public async Task<IActionResult> Get()
+    private readonly ClientService _clientService;
+    public ClientController(ClientService clientService)
     {
-
-      return Ok();
+      _clientService = clientService;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Client>>> GetAllClients()
+    {
+      var clients = await _clientService.GetAllClients();
+      return Ok(clients);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<IEnumerable<Client>>> GetClientById(Guid id)
+    {
+      var clientsFound = _clientService.GetClientById(id);
+
+      return Ok(clientsFound);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Client>> CreateClient([FromBody] ClientRegistrationDto clientDto)
+    {
+      var createdUser = _clientService.AddClient(clientDto);
+      return Ok(createdUser);
+    }
+    [HttpPatch("{id:guid}")]
+    public async Task<ActionResult<Client>> EditClient(Guid id, [FromBody] ClientUpdateDto clientDto)
+    {
+      var updatedClient = _clientService.UpdateClientAsync(id, clientDto);
+      return Ok(updatedClient);
+    }
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult<bool>> DeleteClient(Guid id)
+    {
+      var deleteUser = _clientService.DeleteClientAsync(id);
+      return Ok(deleteUser);
+    }
+
+
   }
 }
